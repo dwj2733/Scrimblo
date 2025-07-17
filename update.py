3887639f8f814f0675beefda40ffc4f8b0eeec8f
dwj2733 <dwj2733@rit.edu>
2025-07-17 12:06:51 -0400

    added dummy response var

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 import requests, schedule
 
 def update():
-    requests.get('http://scrimzone.co/update.php')
+    response = requests.get('http://scrimzone.co/update.php')
     print("Updated")
 
 schedule.every(5).minutes.do(update)
